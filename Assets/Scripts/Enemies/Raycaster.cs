311aa2195af501eb215c95ba3b05511685b295eb
Dorrekens <thomas.dorrekens@student.vinci.be>
2021-10-28 13:58:02 +0200

    new scene

---
+++ Assets/Scripts/Enemies/Raycaster.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private Color color;
+
+    public bool Query()
+    {
+        var ray = new Ray(transform.position + offset, direction);
+        Debug.DrawRay(ray.origin, ray.direction.normalized * maxDistance, Color.red);
+        return Physics.Raycast(ray, maxDistance, layerMask);
+    }
+
+}
